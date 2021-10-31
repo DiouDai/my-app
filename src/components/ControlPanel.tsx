b932c4420e23d7bcc49db150a5d7283f54da2415
Diou <Diou@DESKTOP-SPJ2E7L>
2021-10-31 16:21:00 -0400

    Saving and loading

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -7,15 +7,29 @@ import { useState } from 'react';
 import  { Task as User }  from "editable-dnd-list"
 import {shuffle} from '../utilties/data'
 
+export const LOCAL_STORAGE_USERS = 'ta-trainer-users';
 
-export function ControlPanel({setCard, reveal, answerRevealed}: {setCard:(c:Card)=>void, reveal: (r:boolean)=>void, answerRevealed:boolean}):JSX.Element{
-    const [users,setUsers] = useState<User[]>([
+export const INITIAL_USERS: User[]=
+    [
         {id: '1', text: "Dr.Bart"},
         {id: '2', text: "Ada Bart"},
         {id: '3', text: "Babbage Bart"},
         {id: '4', text: "Pumpkin Bart"},
         {id: '5', text: "Ellie Bart"},
-    ]);
+    ];
+
+    export function getLocalStorageUsers(): User[]{
+        let rawUsers:string | null= localStorage.getItem(LOCAL_STORAGE_USERS);
+        if (rawUsers === null) {
+           return  [...INITIAL_USERS];
+        } else{
+            return JSON.parse(rawUsers);
+        }
+    }
+
+export function ControlPanel({setCard, reveal, answerRevealed}: {setCard:(c:Card)=>void, reveal: (r:boolean)=>void, answerRevealed:boolean}):JSX.Element{
+
+    const [users,setUsers] = useState<User[]>(getLocalStorageUsers());
     
     function setRandomCard(){
         reveal(false);
@@ -26,11 +40,17 @@ export function ControlPanel({setCard, reveal, answerRevealed}: {setCard:(c:Card
         let shuffledUsers: User[] = shuffle(users);
         setUsers([...shuffledUsers]);
     }
+
+    function save(){
+        localStorage.setItem(LOCAL_STORAGE_USERS, JSON.stringify(users))
+    }
+
     return <Col>
     <h1>Control Panel </h1>
     <UserList users={users} setUsers={setUsers}></UserList>
     <Button onClick={setRandomCard} className="m-4">Swap Current Card </Button>
     <Button onClick={()=>reveal(!answerRevealed)}className="m-4">Reveal Answer</Button>
     <Button onClick={shuffleUsers}className="m-4">Shuffle Users</Button>
+    <Button onClick={save} className="m-4" variant='success'>Shuffle Users</Button>
     </Col>
 }
