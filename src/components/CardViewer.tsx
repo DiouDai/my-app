40ec30f4d1e4c8ceb4a499d349ff2abff5222bc1
Diou <Diou@DESKTOP-SPJ2E7L>
2021-10-31 12:48:20 -0400

    Basic card rendering

--- src/components/CardViewer.tsx
+++ src/components/CardViewer.tsx
@@ -1,6 +1,10 @@
 import {Col} from 'react-bootstrap';
+import { Card } from '../interfaces/card';
 
 
-export function CardViewer():JSX.Element{
-    return <Col>CardViewer</Col>
+export function CardViewer({card}:{card:Card}):JSX.Element{
+    return <Col>
+    <h1>CardViewer</h1>
+    <div>Current Card: {card.Prompt} </div>
+    </Col>
 }
